1697f2a9fc632c1e22aea5bf0a73c46e84a2915e
Eugene Prikazchikov <eprikazc@gmail.com>
2015-10-15 16:10:37 +0300

    Implement S3 prefix via BucketDriver

--- depot/io/awss3.py
+++ depot/io/awss3.py
@@ -55,6 +55,23 @@ class S3StoredFile(StoredFile):
         return self._key.generate_url(expires_in=0, query_auth=False)
 
 
+class BucketDriver(object):
+
+    def __init__(self, bucket, prefix):
+        self.bucket = bucket
+        self.prefix = prefix
+
+    def get_key(self, key_name):
+        return self.bucket.get_key('%s%s' % (self.prefix, key_name))
+
+    def new_key(self, key_name):
+        return self.bucket.new_key('%s%s' % (self.prefix, key_name))
+
+    def list_key_names(self):
+        keys = self.bucket.list(prefix=self.prefix)
+        return [k.name[len(self.prefix):] for k in keys]
+
+
 class S3Storage(FileStorage):
     """:class:`depot.io.interfaces.FileStorage` implementation that stores files on S3.
 
@@ -67,7 +84,7 @@ class S3Storage(FileStorage):
     """
 
     def __init__(self, access_key_id, secret_access_key, bucket=None, host=None,
-                 policy=None, encrypt_key=False):
+                 policy=None, encrypt_key=False, prefix=''):
         policy = policy or CANNED_ACL_PUBLIC_READ
         assert policy in [CANNED_ACL_PUBLIC_READ, CANNED_ACL_PRIVATE], (
             "Key policy must be %s or %s" % (CANNED_ACL_PUBLIC_READ, CANNED_ACL_PRIVATE))
@@ -81,15 +98,15 @@ class S3Storage(FileStorage):
         if host is not None:
             kw['host'] = host
         self._conn = S3Connection(access_key_id, secret_access_key, **kw)
-        self._bucket = self._conn.lookup(bucket)
-        if self._bucket is None:
-            self._bucket = self._conn.create_bucket(bucket)
+        bucket = self._conn.lookup(bucket) or self._conn.create_bucket(bucket)
+        self.bucket_driver = BucketDriver(bucket, prefix)
+
 
     def get(self, file_or_id):
         fileid = self.fileid(file_or_id)
         self._check_file_id(fileid)
 
-        key = self._bucket.get_key(fileid)
+        key = self.bucket_driver.get_key(fileid)
         if key is None:
             raise IOError('File %s not existing' % fileid)
 
@@ -121,13 +138,10 @@ class S3Storage(FileStorage):
             key.set_contents_from_string(content, policy=self._policy,
                                          encrypt_key=self._encrypt_key)
 
-    def _get_new_file_id(self):
-        return str(uuid.uuid1())
-
     def create(self, content, filename=None, content_type=None):
         content, filename, content_type = self.fileinfo(content, filename, content_type)
-        new_file_id = self._get_new_file_id()
-        key = self._bucket.new_key(new_file_id)
+        new_file_id = str(uuid.uuid1())
+        key = self.bucket_driver.new_key(new_file_id)
         self.__save_file(key, content, filename, content_type)
         return new_file_id
 
@@ -141,7 +155,7 @@ class S3Storage(FileStorage):
             filename = f.filename
             content_type = f.content_type
 
-        key = self._bucket.get_key(fileid)
+        key = self.bucket_driver.get_key(fileid)
         self.__save_file(key, content, filename, content_type)
         return fileid
 
@@ -149,7 +163,7 @@ class S3Storage(FileStorage):
         fileid = self.fileid(file_or_id)
         self._check_file_id(fileid)
 
-        k = self._bucket.get_key(fileid)
+        k = self.bucket_driver.get_key(fileid)
         if k:
             k.delete()
 
@@ -157,11 +171,11 @@ class S3Storage(FileStorage):
         fileid = self.fileid(file_or_id)
         self._check_file_id(fileid)
 
-        k = self._bucket.get_key(fileid)
+        k = self.bucket_driver.get_key(fileid)
         return k is not None
 
     def list(self):
-        return [key.name for key in self._bucket.list()]
+        return self.bucket_driver.list_key_names()
 
     def _check_file_id(self, file_id):
         # Check that the given file id is valid, this also
@@ -170,24 +184,3 @@ class S3Storage(FileStorage):
             uuid.UUID('{%s}' % file_id)
         except:
             raise ValueError('Invalid file id %s' % file_id)
-
-
-class S3PrefixedStorage(S3Storage):
-
-    def __init__(self, *args, **kwargs):
-        self._prefix = kwargs.pop('prefix')
-        super(S3PrefixedStorage, self).__init__(*args, **kwargs)
-
-    def _get_new_file_id(self):
-        new_file_id = super(S3PrefixedStorage, self)._get_new_file_id()
-        return '%s%s' % (self._prefix, new_file_id)
-
-    def _check_file_id(self, file_id):
-        # Check that the given file id is valid, this also
-        # prevents unsafe paths.
-        if file_id.startswith(self._prefix):
-            file_id = file_id[len(self._prefix):]
-        super(S3PrefixedStorage, self)._check_file_id(file_id)
-
-    def list(self):
-        return [key.name for key in self._bucket.list(prefix=self._prefix)]
