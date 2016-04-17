d19146d7e172eb17bd776a149dc0008043c2eea4
Eugene Prikazchikov <eprikazc@gmail.com>
2016-04-17 08:35:13 +0300

    Fix test_content_disposition for Python3.2

--- tests/test_awss3_storage.py
+++ tests/test_awss3_storage.py
@@ -6,7 +6,7 @@ import requests
 
 from nose import SkipTest
 
-from depot._compat import PY2
+from depot._compat import PY2, unicode_text
 
 
 S3Storage = None
@@ -75,7 +75,7 @@ class TestS3FileStorage(object):
     def test_content_disposition(self):
         if not PY2:
             raise SkipTest('Test is for Python2.X only')
-        file_id = self.fs.create(b'content', u'test.txt', 'text/plain')
+        file_id = self.fs.create(b'content', unicode_text('test.txt'), 'text/plain')
         test_file = self.fs.get(file_id)
         response = requests.get(test_file.public_url)
         assert response.headers['Content-Disposition'] == "inline;filename=test.txt;filename*=utf-8''test.txt"
