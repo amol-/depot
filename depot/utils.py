b0a6d22a4e84cc8b69120c9df1a263d258d1ccf7
Pavel Gribanov <rapthead@gmail.com>
2016-09-08 14:49:05 +0500

    fix some tests

--- depot/utils.py
+++ depot/utils.py
@@ -4,5 +4,5 @@ from ._compat import percent_encode
 
 def make_content_disposition(disposition, fname):
     rfc6266_part = "filename*=utf-8''%s" % (percent_encode(fname, safe='!#$&+-.^_`|~', encoding='utf-8'), )
-    ascii_part = "filename=\"%s\"" % (unidecode(fname), )
+    ascii_part = 'filename="%s"' % (unidecode(fname), )
     return ';'.join((disposition, ascii_part, rfc6266_part))
