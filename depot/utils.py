f8c9b8f22956b07c950e1aee8b37ce8b036483b8
Jim McCusker <mccusker@gmail.com>
2020-07-24 14:39:37 -0400

    Code improvements for quality checks.

--- depot/utils.py
+++ depot/utils.py
@@ -2,15 +2,15 @@ from ._compat import percent_encode
 
 try:
     from unidecode import unidecode as fix_chars
-except:
+except ImportError:
     import sys
     from unicodedata import normalize
     if (sys.version_info > (3, 0)):
         from urllib.parse import quote
     else:
         from urllib import quote
-    def fix_chars(str):
-        return quote(normalize('NFKC', str))
+    def fix_chars(string):
+        return quote(normalize('NFKC', string))
 
 def make_content_disposition(disposition, fname):
     rfc6266_part = "filename*=utf-8''%s" % (percent_encode(fname, safe='!#$&+-.^_`|~', encoding='utf-8'), )
