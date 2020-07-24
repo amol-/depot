b9ddbd2ac5da081500d2325c691d2524e1aa14e8
Jim McCusker <mccusker@gmail.com>
2020-07-24 14:30:53 -0400

    percent encode doesn't quite do the right thing, so adding urllib.quote in directly.

--- depot/utils.py
+++ depot/utils.py
@@ -3,9 +3,14 @@ from ._compat import percent_encode
 try:
     from unidecode import unidecode as fix_chars
 except:
+    import sys
     from unicodedata import normalize
+    if (sys.version_info > (3, 0)):
+        from urllib.parse import quote
+    else:
+        from urllib import quote
     def fix_chars(str):
-        return percent_encode(normalize('NFKC', str))
+        return quote(normalize('NFKC', str))
 
 def make_content_disposition(disposition, fname):
     rfc6266_part = "filename*=utf-8''%s" % (percent_encode(fname, safe='!#$&+-.^_`|~', encoding='utf-8'), )
